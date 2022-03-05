e8ba11e2d1a053336f30cebe0f28978cc2134835
Orbey Paltacı <orbeypaltaci@gmail.com>
2022-03-05 16:50:14 +0300

    Controller created
    Car controller created to get car information from the database and return it in json

---
+++ WarehouseApplication/WarehouseApplication.WebApi/CarTableModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace WarehouseApplication.WebApi
+{
+    public class CarTableModel
+    {
+        public string Location { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int YearModel { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string WarehouseName { get; set; }
+
+    }
+}
---
+++ WarehouseApplication/WarehouseApplication.WebApi/Controllers/CarController.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WarehouseApplication.Data;
+
+namespace WarehouseApplication.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase
+    {
+        private readonly ILogger<CarController> _logger;
+        private readonly WarehouseContext _context;
+
+        public CarController(ILogger<CarController> logger, WarehouseContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Cars()
+        {
+            var query = await _context.Car
+                .Include(x => x.Warehouse)
+                .Select(y => new CarTableModel
+                {
+                    DateAdded = y.DateAdded,
+                    Make = y.Make,
+                    Model = y.Model,
+                    Price = y.Price,
+                    Location = y.Location,
+                    YearModel = y.YearModel,
+                    WarehouseName = y.Warehouse.Name
+                })
+                .ToArrayAsync();
+
+            var jsonData = JsonSerializer.Serialize(query);
+
+            return new OkObjectResult(jsonData);
+        }
+
+    }
+}
