ee6f9a6f72911dbdd4f299e52aa13a61550ae3e4
Orbey Paltacı <orbeypaltaci@gmail.com>
2022-03-05 16:16:21 +0300

    Context and Entities
    Created the db context and entities

---
+++ WarehouseApplication/WarehouseApplication.Data/Entities/Car.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace WarehouseApplication.Data.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int YearModel { get; set; }
+        public decimal Price { get; set; }
+        public bool Licensed { get; set; }
+        public DateTime DateAdded { get; set; }
+        public Warehouse Warehouse { get; set; }
+        public int WarehouseId { get; set; }
+
+    }
+}
---
+++ WarehouseApplication/WarehouseApplication.Data/Entities/Warehouse.cs
@@ -0,0 +1,11 @@
+namespace WarehouseApplication.Data.Entities
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+
+    }
+}
